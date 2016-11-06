11a3af920e79c6f446987f37d07a12c4dbbec438
Santosh Ghimire <santosh.ghimire33@gmail.com>
2016-11-06 15:23:16 +0545

    changes for celery scheduler

--- README.md
+++ README.md
@@ -13,3 +13,21 @@
 5. python manage.py migrate
 6. python manage.py runserver
 7. View 127.0.0.1:8000 on your browser.
+
+## Periodic Task
+Gets page info, page posts and posts like from facebook api
+and updates the database every hour.
+
+To run the periodic task, you need to do the following steps.
+1. Rabbitmq: Install Rabbitmq
+2. sudo rabbitmq-server -detached
+3. sudo rabbitmqctl add_user santosh mirage
+4. sudo rabbitmqctl add_vhost myvhost
+5. sudo rabbitmqctl set_permissions -p myvhost santosh ".*" ".*" ".*"
+6. sudo rabbitmq-server -detached
+
+7. python manage.py runserver
+8. celery -A fbstats beat
+9. celery -A fbstats worker -l debug
+
+This will update the database with new posts every hour at 0 minutes.
--- fbstats/settings.py
+++ fbstats/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import djcelery
 
+djcelery.setup_loader()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
+    'djcelery',
     # local apps
     'mainapp',
 ]
@@ -182,3 +184,12 @@ ACCOUNT_SIGNUP_PASSWORD_VERIFICATION = False
 # END AUTHENTICATION CONFIGURATION
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'account_login'
+
+# celery settings
+BROKER_URL = "amqp://santosh:mirage@localhost:5672/myvhost"
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Kathmandu'
+CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
+CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
